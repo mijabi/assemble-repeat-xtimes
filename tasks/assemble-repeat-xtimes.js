a5e61975e695bd4217fee8ba470df7ed452dd346
sato-masay <sato-masay@gnavi.co.jp>
2015-10-21 15:54:04 +0900

    remove comment

--- tasks/assemble-repeat-xtimes.js
+++ tasks/assemble-repeat-xtimes.js
@@ -1,5 +1,5 @@
 module.exports.register = function (Handlebars, options)  {
-  Handlebars.registerHelper("assemble-repeat-xtimes", function(index_count, mod, block) { // 指定回数リピートするカスタムヘルパー
+  Handlebars.registerHelper("assemble-repeat-xtimes", function(index_count, mod, block) {
     if(parseInt(index_count) % (mod) === 0){
       return block.fn(this);
     }
