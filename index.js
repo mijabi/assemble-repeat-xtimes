8fc9fcf7118461fe0d088da52e73787a0866c666
sato-masay <sato-masay@gnavi.co.jp>
2015-10-21 17:35:32 +0900

    edit
    avoid match 1st time loop

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports.register = function (Handlebars, options)  {
   Handlebars.registerHelper("assemble-repeat-xtimes", function(index_count, mod, block) {
-    if(parseInt(index_count) % (mod) === 0){
+    if(parseInt(index_count) % (mod) === 0 && index_count !== 0){
       return block.fn(this);
     }
   });
--- tasks/assemble-repeat-xtimes.js
+++
@@ -1,7 +0,0 @@
-module.exports.register = function (Handlebars, options)  {
-  Handlebars.registerHelper("assemble-repeat-xtimes", function(index_count, mod, block) {
-    if(parseInt(index_count) % (mod) === 0){
-      return block.fn(this);
-    }
-  });
-};
